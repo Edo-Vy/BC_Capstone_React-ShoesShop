e096f27c076bcaad22f421786f5ea6874f0f7f92
Edo-Vy <tranthaovy8898@gmai.com>
2023-09-17 23:03:45 +0700

    search-sc-d1

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,39 +1,42 @@
 // rfc
-import React from 'react'
+import React from "react";
 
 export default function Search() {
   return (
-    <div className='search'>
-      <div className='main__wrap'>
-        <div className='search__header'>
-          <form className='main__search'>
-            <div className='main__right form-group'>
-                <p className='search__title'>Search</p>
-                <input className='search__form form-control' placeholder='Search Product' name='search'/>
+    <div className="search">
+      <div className="main__wrap">
+        <div className="search__header">
+          <form className="main__search">
+            <div className="main__right form-group">
+              <p className="search__title">Search</p>
+              <input
+                className="search__form form-control"
+                placeholder="Search Product"
+                name="search"
+              />
             </div>
-            <div className='main__left'>
-              <button className='btn__search'>Search</button>
+            <div className="main__left">
+              <button className="btn__search">Search</button>
             </div>
           </form>
         </div>
         {/* ---- */}
-        <div className='search__result'>
-          <div className='body__wrap'>
-            <div className='body__title'>
-              <h3 className='title'>Search result</h3>
+        <div className="search__result">
+          <div className="body__wrap">
+            <div className="body__title">
+              <h3 className="title">Search result</h3>
             </div>
-            <div className='body__option'>
-              <div className='search__price '>
-                <div className='price__decrease'>
-                  <button className='btn__decrease'>decrease</button>
-                </div>
-                <div className='price__ascending'>
-                  <button className='btn__ascending'>ascending</button>
+            <div className="body__option">
+              <div className="search__price ">
+                <p className="price__title">Price</p>
+                <div className="price__button">
+                  <button className="btn__decrease">decrease</button>
+                  <button className="btn__ascending">ascending</button>
                 </div>
               </div>
-              <div className='option__branchs'>
-                <p className='branch__title'>Branchs</p>
-                <select className='select__branchs'>
+              <div className="option__branchs">
+                <p className="branch__title">Branchs</p>
+                <select className="select__branchs">
                   <option>Adidas</option>
                   <option>Vans</option>
                   <option>Nike</option>
@@ -41,8 +44,8 @@ export default function Search() {
               </div>
             </div>
             {/* === */}
-            <div className='search__product'>
-              <div className='row'>
+            <div className="search__product">
+              <div className="row">
                 <div>Call api</div>
               </div>
             </div>
@@ -50,5 +53,5 @@ export default function Search() {
         </div>
       </div>
     </div>
-  )
+  );
 }
