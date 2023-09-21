31133b799d6b3b5ba6d43d0eeb7615484c359b7c
Edo-Vy <tranthaovy8898@gmai.com>
2023-09-21 15:47:48 +0700

    d-cj-1

--- src/templates/HomeTemplate/HomeTemplate.jsx
+++ src/templates/HomeTemplate/HomeTemplate.jsx
@@ -1,18 +1,15 @@
 // rfc
-import React, { useEffect } from "react";
+import React from "react";
 
 import { Outlet } from "react-router-dom";
 import BackToTop from "../../components/BackToTop/BackToTop";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 
-
-
 export default function HomeTemplate(props) {
-
   return (
     <div id="back_totop">
-      <BackToTop/>
+      <BackToTop />
       <header>
         <Header />
       </header>
